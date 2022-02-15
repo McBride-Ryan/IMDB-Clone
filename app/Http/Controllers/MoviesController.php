3a96af7aaf6bb0069d7a844fb593b1823278ea48
Ryan McBride <ryanmcbride@ryans-mbp.myfiosgateway.com>
2022-02-15 07:57:19 -0500

    The Movie Box

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -26,8 +26,6 @@ class MoviesController extends Controller
             return [$genre['id'] => $genre['name']];
         });
 
-        dump($nowPlayingMovies);
-
         return view('index', [
             'popularMovies' => $popularMovies,
             'genres' => $genres,
@@ -48,10 +46,16 @@ class MoviesController extends Controller
     }
 
 
-    public function show($popularMovies)
+    public function show($id)
     {
+        $movie = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
+            ->json();
+
+        dump($movie);
+
         return view('details', [
-            'popularMovies' => $popularMovies
+            'movie' => $movie
         ]);
     }
 
